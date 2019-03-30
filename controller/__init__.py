a64fafa095a8bf571bd3336d7409ce953cf38383
James Abernathy <jabernat@gmail.com>
2019-03-30 02:08:21 -0700

    Packages now include their contained modules using `__init__.py`.

---
+++ controller/__init__.py
@@ -0,0 +1,11 @@
+"""EasyMoney's MVC controller `sim_controller` and supporting modules."""
+
+
+__copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Gerritsen'
+__license__ = 'MIT'
+
+
+import controller.sim_controller
+
+import controller.market_datasource
+import controller.market_updater
---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+"""EasyMoney's MVC model `sim_model` and supporting modules."""
+
+
+__copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Gerritsen'
+__license__ = 'MIT'
+
+
+import model.sim_model
+
+import model.stock_market
+import model.trader
+import model.trader_account
