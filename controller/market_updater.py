a9cc83eff4398c36282bac06783df9da742d5610
tylergerritsen <tylergerritsen>
2019-04-09 01:30:14 -0700

    updates: - added docstrings for updater attributes - updated method signatures to match professional conventions - fixed semicolon spaces - changed string comparison "is" to "==" for reliability purposes - updated docstring comments in general

--- controller/market_updater.py
+++ controller/market_updater.py
@@ -11,16 +11,6 @@ from kivy.clock import (
         #MARKET_UPDATER_RESET
 
 class MarketUpdater(object):
-    '''
-    The following data members are used to manage the module. The valid states
-    are playing, paused, and reset. The controller member provided is initialized
-    by the constructor. The event member is used by Kivy to implement a timed
-    method call event every 12 seconds
-    '''
-    state : str
-    controller : 'SimController'
-    event : ClockEvent
-
     """
     The Market Updater periodically gets data from the Datasource component and
     channels it to the StockMarket component in the SimModel module. It also
@@ -31,63 +21,91 @@ class MarketUpdater(object):
     event that is broadcast to the Window View module.
     """
 
-    def __init__ (self, source: 'SimController') -> None:
+    market_updater_state: str
+    """
+    state attribute represents the current running status of the updater; paused,
+    playing, or reset
+    """
+    parent_application_controller: 'SimController'
+    """
+    this controller attribute will be the simulation controller object that 
+    contains this updater. Using this object will allow for simulation
+    updates and information retrieval from the datasource
+    """
+    sheduled_simulation_update_event: ClockEvent
+    """
+    Kivy provides a ClockEvent ADT to work with. This datatype represents an 
+    event that can be scheduled to occur at regular intervals. This application
+    uses this data type to schedule a call to a function which retrieves
+    symbol data from the market datasource module and updates the data in the
+    simulation model accordingly.
+    """
+
+    def __init__(self,
+                  controller: 'SimController'
+                 ) -> None:
         """
         The constructor for MarketUpdater . All new MarketUpdater s start in a
         paused state.
         """
-        self.controller = source
-        self.state = "paused"
+        self.parent_application_controller = controller
+        self.market_updater_state = "paused"
 
     
-    def is_playing (self) -> bool:
+    def is_playing (self
+                    ) -> bool:
 
         """
         This method returns true when MarketUpdater is in a play state.
         """
-        if self.state is "playing":
+        if self.market_updater_state == "playing":
             return True
         else:
             return False
     
-    def pause(self) -> None:
+    def pause(self
+              ) -> None:
         """
         This method sets this MarketUpdater to a pause state. While paused,
         new data samples will no longer be sent to the SimModel from the
         data source.
         """
-        self.event.cancel()
-        self.state = "paused"
+        self.sheduled_simulation_update_event.cancel()
+        self.market_updater_state = "paused"
         # TODO MARKET_UPDATER_PAUSED
     
-    def play(self) -> None:
+    def play(self
+             ) -> None:
         """
         This method sets this MarketUpdater to a play state. While playing, new
         data samples may be periodically sent to the SimModel from the
         datasource.
         """
-        self.event = Clock.schedule_interval(self.get_prices(), 12)
-        self.state = "playing"
+        self.sheduled_simulation_update_event = Clock.schedule_interval(
+            self.update_model_prices_from_datasource(), 12)
+        self.market_updater_state = "playing"
         # TODO MARKET_UPDATER_PLAYING
 
     
-    def reset(self) -> None:
+    def reset(self
+              ) -> None:
         """
-        This method calls resets the market and trader accounts, unconfirms the
+        This method call resets the market and trader accounts, unconfirms the
         datasource, and pauses this MarketUpdater.
         """
-        self.state = "reset"
+        self.market_updater_state = "reset"
 
         #TODO MARKET_UPDATER_RESET
 
-    def get_prices(self) -> None:
+    def update_model_prices_from_datasource(self
+                                            ) -> None:
         '''
         Gets the current prices from the datasource, if an update is confirmed,
         and calls appropriate simulation module functions to update those prices.
         '''
-        if self.controller.can_confirm():
-            prices = self.controller.get_datasource().get_next_prices()
-            self.controller.get_model().get_stock_market().add_next_prices(prices[0], prices[1])
+        if self.parent_application_controller.is_confirmed():
+            prices = self.parent_application_controller.get_datasource().get_next_prices()
+            self.parent_application_controller.get_model().get_stock_market().add_next_prices(prices[0], prices[1])
 
 
 
