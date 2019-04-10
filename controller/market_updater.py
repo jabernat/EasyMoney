938e07aecaedf6fa891f3405483c2a0e1d6ab291
James Abernathy <jabernat@gmail.com>
2019-04-10 00:17:53 -0700

    Significant overhaul of `MarketUpdater` to correctly interact with the `model`.
    * State is now represented with an `enum.Enum` instead of a `str`.
    * Renamed some members for brevity and clarity.
    * Added an `.is_paused()` method, to distinguish between paused and reset when `not .is_playing()`.
    * Raise an exception if the datasource unexpectedly unconfirms.
    * Update immediately upon `.play()`, instead of waiting for the next timer tick.
    * Increased the update rate to once per second.
    * Clear the timer update object once canceled.
    * Implemented `.reset()`'s documented behavior.
    * Changed style for consistency with the rest of the project.

--- controller/market_updater.py
+++ controller/market_updater.py
@@ -1,110 +1,177 @@
+"""Defines `MarketUpdater` and supporting classes."""
+
+
+__copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Gerritsen'
+__license__ = 'MIT'
+
+
+import enum
 
 from kivy.clock import (
     Clock, ClockEvent)
 
 # Local package imports at end of file to resolve circular dependencies
 
-#TODO: 
-    #Events:
-        #MARKET_UPDATER_PAUSED
-        #MARKET_UPDATER_PLAYING
-        #MARKET_UPDATER_RESET
 
-class MarketUpdater(object):
-    """
-    The Market Updater periodically gets data from the Datasource component and
-    channels it to the StockMarket component in the SimModel module. It also
-    receives state calls from the Window View module to start and pause the data
-    flow to the Simulation Model. This component also passes calls to reset the
-    simulation to the Simulation Model from the Window View module. Whether the
-    MarketUpdater has been started, paused, or reset, each has a respective
-    event that is broadcast to the Window View module.
+
+
+class UnexpectedDatasourceUnconfirmError(RuntimeError):
+    """An exception raised when the `MarketDatasource` becomes unconfirmed
+    while the `MarketUpdater` is playing or paused.
     """
 
-    _market_updater_state: str
-    """
-    state attribute represents the current running status of the updater; paused,
-    playing, or reset
-    """
-    _parent_application_controller: 'SimController'
-    """
-    this controller attribute will be the simulation controller object that 
-    contains this updater. Using this object will allow for simulation
-    updates and information retrieval from the datasource
-    """
-    _sheduled_simulation_update_event: ClockEvent
-    """
-    Kivy provides a ClockEvent ADT to work with. This datatype represents an 
-    event that can be scheduled to occur at regular intervals. This application
-    uses this data type to schedule a call to a function which retrieves
-    symbol data from the market datasource module and updates the data in the
-    simulation model accordingly.
-    """
+    updater_state: 'MarketUpdater.State'
+    """The state of the `MarketUpdater` when the datasource unconfirmed."""
 
     def __init__(self,
-                  controller: 'SimController'
-                 ) -> None:
-        """
-        The constructor for MarketUpdater . All new MarketUpdater s start in a
-        paused state.
-        """
-        self.parent_application_controller = controller
-        self.market_updater_state = "paused"
+        updater_state: 'MarketUpdater.State'
+    ) -> None:
+        self.updater_state = updater_state
+        super().__init__('The market datasource was unexpectedly unconfirmed '
+            'while its market updater was in the {!r} state.'.format(
+                updater_state.name))
 
-    
-    def is_playing (self
-                    ) -> bool:
+
+
+
+class MarketUpdater(object):
+    """Periodically gets data from a price datasource and channels it into the
+    `model.StockMarket`. The data flow starts out stopped (called reset), and
+    can be started with `.play()` and paused with `.pause()`.
+    """
+
+
+    class State(enum.Enum):
+        """Enumeration of all possible `MarketUpdater` states."""
+        RESET = 0
+        """State of `MarketUpdater` when stopped."""
+
+        PLAYING = 1
+        """State of `MarketUpdater` when periodically feeding prices into the
+        `model.StockMarket`.
         """
-        This method returns true when MarketUpdater is in a play state.
+
+        PAUSED = 2
+        """State of the `MarketUpdater` when temporarilly halted after having
+        fed some data into the `model.StockMarket`.
         """
-        return self.market_updater_state == "playing"
-    
-    def pause(self
-              ) -> None:
-        """
-        This method sets this MarketUpdater to a pause state. While paused,
-        new data samples will no longer be sent to the SimModel from the
-        data source.
-        """
-        self.sheduled_simulation_update_event.cancel()
-        self.market_updater_state = "paused"
-        # TODO MARKET_UPDATER_PAUSED
-    
+
+
+    _parent_controller: 'SimController'
+    """This updater's owning simulation controller."""
+
+    _state: MarketUpdater.State
+    """Status of this updater controlling its activity."""
+
+    _update_timer: typing.Optional[ClockEvent]
+    """A timer updater started by Kivy while playing, or `None` in other
+    states.
+    """
+
+    #TODO: Events:
+    #   MARKET_UPDATER_PAUSED
+    #   MARKET_UPDATER_PLAYING
+    #   MARKET_UPDATER_RESET
+
+
+    def __init__(self,
+        controller: 'SimController'
+    ) -> None:
+        """Start this new `MarketUpdater` in a paused state."""
+        self._parent_controller = controller
+        self._state = self.State.RESET
+        self._update_timer = None
+
+
+    def is_playing(self
+    ) -> bool:
+        """Return `True` if this `MarketUpdater` is updating."""
+        return self._state == self.State.PLAYING
+
     def play(self
-             ) -> None:
+    ) -> None:
+        """Start or resume periodically delivering prices to the
+        `model.StockMarket` from the `_controller`'s `MarketDatasource`.
         """
-        This method sets this MarketUpdater to a play state. While playing, new
-        data samples may be periodically sent to the SimModel from the
-        datasource.
-        """
-        self.sheduled_simulation_update_event = Clock.schedule_interval(
-            self.update_model_prices_from_datasource, 12)
-        self.market_updater_state = "playing"
+        if self.is_playing():
+            return  # Already playing
+
+        datasource = self._parent_controller.get_datasource()
+        if self.is_paused():
+            if not datasource.is_confirmed():
+                self.reset()
+                raise UnexpectedDatasourceUnconfirmError(self.State.PAUSED)
+        else:  # Currently reset
+            datasource.confirm()
+
+        self._state = self.State.PLAYING
         # TODO MARKET_UPDATER_PLAYING
 
-    
-    def reset(self
-              ) -> None:
-        """
-        This method call resets the market and trader accounts, unconfirms the
-        datasource, and pauses this MarketUpdater.
-        """
-        self.market_updater_state = "reset"
+        # Resume periodic updates
+        self._update_timer = Clock.schedule_interval(
+            self._add_market_prices_from_datasource, 1.0)
+        # Make first update immediately
+        self._add_market_prices_from_datasource()
 
+
+    def is_paused(self
+    ) -> bool:
+        """Return `True` if this `MarketUpdater` was playing but was then
+        paused.
+        """
+        return self._state == self.State.PAUSED
+
+    def pause(self
+    ) -> None:
+        """Pause this `MarketUpdater`, halting the flow of prices from
+        `MarketDatasource` to `model.StockMarket`.
+        """
+        if not self.is_playing():
+            return  # No activity to pause
+
+        if self._update_timer is not None:
+            self._update_timer.cancel()
+            self._update_timer = None
+
+        self._state = self.State.PAUSED
+        # TODO MARKET_UPDATER_PAUSED
+
+
+    def reset(self
+    ) -> None:
+        """Stops any paused or playing updates, resets the `model`'s market and
+        trader accounts, and unlocks the datasource.
+        """
+        if not (self.is_playing() or self.is_paused()):
+            return  # Already reset
+
+        self.pause()  # Stop updates if playing
+
+        self._state = self.State.RESET
         #TODO MARKET_UPDATER_RESET
 
-    def update_model_prices_from_datasource(self
-                                            ) -> None:
-        '''
-        Gets the current prices from the datasource, if an update is confirmed,
-        and calls appropriate simulation module functions to update those prices.
-        '''
-        if self.parent_application_controller.is_confirmed():
-            prices = self.parent_application_controller.get_datasource().get_next_prices()
-            if prices != None:
-                self.parent_application_controller.get_model().get_stock_market().add_next_prices(prices[0], prices[1])
-            else:
-                self.pause()
+        self._parent_controller.get_model().clear()
+        self._parent_controller.get_datasource().unconfirm()
+
+
+    def _add_market_prices_from_datasource(self
+    ) -> None:
+        """Get the current prices from the datasource if an update is
+        confirmed, and call appropriate simulation module functions to update
+        those prices.
+        """
+        if not self._parent_controller.is_confirmed():
+            self.reset()
+            raise UnexpectedDatasourceUnconfirmError(self.State.PLAYING)
+
+        time_and_prices = self._parent_controller.get_datasource().get_next_prices()
+        if not time_and_prices:  # Ran out of data
+            self.pause()
+            return
+
+        market = self._parent_controller.get_model().get_stock_market()
+        market.add_next_prices(*time_and_prices)
+
 
 
 
