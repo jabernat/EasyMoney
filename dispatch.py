c33b8cf31fbb49af64c78e0235fd734fbc99d5f0
James Abernathy <jabernat@gmail.com>
2019-04-10 23:24:17 -0700

    Added `dispatch` type annotations.

--- dispatch.py
+++ dispatch.py
@@ -10,37 +10,41 @@ import typing
 
 
 
-class Event(object):
+class EventListeners(object):
     """Holds references to event names and subscribed listeners.
 
     This is used internally by :class:`Dispatcher`.
     """
     __slots__ = ('name', 'listeners')
 
+    name: str
+
+    listeners: typing.List[typing.Callable]
+
 
     def __init__(self,
-        name
-    ):
+        name: str
+    ) -> None:
         self.name = name
         self.listeners = []
 
 
     def add_listener(self,
-        callback
-    ):
+        callback: typing.Callable
+    ) -> None:
         self.listeners.append(callback)
 
 
     def remove_listener(self,
-        callback
-    ):
+        callback: typing.Callable
+    ) -> None:
         self.listeners.remove(callback)
 
 
     def __call__(self,
-        *args,
-        **kwargs
-    ):
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> bool:
         """Dispatch the event to listeners.
 
         Called by :meth:`~Dispatcher.emit`
@@ -48,15 +52,16 @@ class Event(object):
         for callback in self.listeners:
             if callback(*args, **kwargs) is False:
                 return False
+        return True  # All listeners notified
 
 
     def __repr__(self
-    ):
+    ) -> str:
         return '<{}: {}>'.format(self.__class__, self)
 
 
     def __str__(self
-    ):
+    ) -> str:
         return self.name
 
 
@@ -65,87 +70,80 @@ class Event(object):
 class Dispatcher(object):
     """Core class used to enable all functionality in the library.
 
-    Interfaces with :class:`Event` objects upon instance creation.
+    Interfaces with :class:`EventListeners` objects upon instance creation.
 
-    Events can be created by calling :meth:`register_event` or by the subclass
+    Events can be created by calling :meth:`register_events` or by the subclass
     definition::
 
         class Foo(Dispatcher):
-            _events_ = ['awesome_event', 'on_less_awesome_event']
+            EVENTS = ['awesome_event', 'on_less_awesome_event']
 
     Once defined, an event can be dispatched to listeners by calling :meth:`emit`.
     """
-    __initialized_subclasses = set()
-    __skip_initialized = True
+    __initialized_subclasses: typing.ClassVar[typing.Set[typing.Type['Dispatcher']]] = set()
+
+    __events_combined: typing.ClassVar[typing.Set[str]]
+
+    __event_listeners: typing.Dict[str, EventListeners]
 
 
     def __new__(cls,
-        *args,
-        **kwargs
-    ):
-        def iter_bases(_cls):
-            if _cls is not object:
-                yield _cls
-                for b in _cls.__bases__:
-                    for _cls_ in iter_bases(b):
-                        yield _cls_
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> 'Dispatcher':
+        def iter_bases(cls_):
+            if cls_ is not object:
+                yield cls_
+                for base in cls_.__bases__:
+                    for base_cls in iter_bases(base):
+                        yield base_cls
 
-        skip_initialized = Dispatcher._Dispatcher__skip_initialized
-        if not skip_initialized or cls not in Dispatcher._Dispatcher__initialized_subclasses:
-            events = set()
-            for _cls in iter_bases(cls):
-                for attr in dir(_cls):
-                    _events = getattr(_cls, '_events_', [])
-                    events |= set(_events)
-            cls._EVENTS_ = events
-            if skip_initialized:
-                Dispatcher._Dispatcher__initialized_subclasses.add(cls)
-        obj = super(Dispatcher, cls).__new__(cls)
-        obj._Dispatcher__init_events()
-        return obj
+        if cls not in Dispatcher.__initialized_subclasses:
+            events_combined: typing.Set[str] = set()
+            for base_cls in iter_bases(cls):
+                try:
+                    events_combined |= set(base_cls.EVENTS)
+                except AttributeError:
+                    pass
+            cls.__events_combined = events_combined
+            Dispatcher.__initialized_subclasses.add(cls)
+
+        instance = super(Dispatcher, cls).__new__(cls, *args, **kwargs)  # type: ignore
+        instance.__event_listeners = {}
+        instance.register_events(*cls.__events_combined)
+        return instance
 
 
     def __init__(self,
-        *args,
-        **kwargs
-    ):
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> None:
         # Everything is handled by __new__
-        # This is only here to prevent exceptions being raised
         pass
 
 
-    def __init_events(self
-    ):
-        if hasattr(self, '_Dispatcher__events'):
-            return
-        self.__events = {}
-        for name in self._EVENTS_:
-            self.__events[name] = Event(name)
-
-
-    def register_event(self,
-        *names
-    ):
+    def register_events(self,
+        *names: str
+    ) -> None:
         """Register new events after instance creation.
 
         Args:
             *names (str): Name or names of the events to register
         """
-        for name in names:
-            if name not in self.__events:
-                self.__events[name] = Event(name)
+        self.__event_listeners.update((name, EventListeners(name))
+            for name in names if name not in self.__event_listeners)
 
 
     def bind(self,
-        **kwargs
-    ):
+        **event_callbacks: typing.Callable
+    ) -> None:
         """Subscribe to events.
 
-        Keyword arguments are used with the Event names as keys
+        Keyword arguments are used with the event names as keys
         and the callbacks as values::
 
             class Foo(Dispatcher):
-                _events_ = ['awesome_event']
+                EVENTS = ['awesome_event']
 
             foo = Foo()
 
@@ -155,57 +153,68 @@ class Dispatcher(object):
         The callbacks are stored as weak references and their order is not
         maintained relative to the order of binding.
         """
-        for name, cb in kwargs.items():
-            self.__events[name].add_listener(cb)
+        for name, callback in event_callbacks.items():
+            self.__event_listeners[name].add_listener(callback)
 
 
     def unbind(self,
-        *args
-    ):
+        *callbacks: typing.Callable
+    ) -> None:
         """Unsubscribe from events.
 
-        Multiple arguments can be given. Each of which can be either the method
-        that was used for the original call to :meth:`bind` or an instance
-        object.
-
-        If an instance of an object is supplied, any previously bound Events
-        will be 'unbound'.
+        Multiple bound methods can be removed at once.
         """
-        for e in self.__events.values():
-            for arg in args:
-                e.remove_listener(arg)
+        for listeners in self.__event_listeners.values():
+            for callback in callbacks:
+                listeners.remove_listener(callback)
 
 
     def emit(self,
-        name,
-        *args,
-        **kwargs
-    ):
-        """Dispatche an event to any subscribed listeners.
+        name: str,
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> bool:
+        """Dispatch an event to any subscribed listeners.
 
         Note:
             If a listener returns :obj:`False`, the event will stop dispatching to
             other listeners. Any other return value is ignored.
 
         Args:
-            name (str): The name of the :class:`Event` to dispatch
+            name (str): The name of the event to dispatch
             *args (Optional): Positional arguments to be sent to listeners
             **kwargs (Optional): Keyword arguments to be sent to listeners
         """
-        return self.__events[name](*args, **kwargs)
+        return self.__event_listeners[name](*args, **kwargs)
 
 
-    def get_dispatcher_event(self,
-        name
-    ):
-        """Retrieve an Event object by name.
+    def get_event_listeners(self,
+        name: str
+    ) -> EventListeners:
+        """Retrieve an EventListeners object by name.
 
         Args:
-            name (str): The name of the :class:`Event` object to retrieve
+            name (str): The name of the :class:`EventListeners` object to retrieve
 
         Returns:
-            The :class:`Event` instance for the event definition
+            The :class:`EventListeners` instance for the event definition
 
         .. versionadded:: 0.1.0
         """
-        return self.__events[name]
+        return self.__event_listeners[name]
+
+
+
+
+if __name__ == '__main__':
+    # Simple test of event dispatch
+    class Test(Dispatcher):
+        EVENTS = ['EVENT_A', 'EVENT_B']
+        def __init__(self):
+            pass
+
+    test = Test()
+    test.bind(EVENT_A=print, EVENT_B=print)
+    test.emit('EVENT_A', 'Event A fired.')
+    test.emit('EVENT_B', 'Event B fired.')
+    print('test =', dir(test))
