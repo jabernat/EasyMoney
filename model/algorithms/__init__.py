ca76214a793902733fa1d8997bab0a70cf4e0b6a
James Abernathy <jabernat@gmail.com>
2019-04-13 01:05:05 -0700

    Added a simple trading algorithm stub, `model.algorithms.trader_example`.

---
+++ model/algorithms/__init__.py
@@ -0,0 +1,9 @@
+"""Package of `model.Trader` subclasses that implement trading algorithms."""
+
+
+__copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Gerritsen'
+__license__ = 'MIT'
+
+
+from model.algorithms import (
+    trader_example)
