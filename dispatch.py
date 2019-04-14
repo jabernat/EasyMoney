2108fe69c1efe1532b668e5ad3161a05ac2a34f4
James Abernathy <jabernat@gmail.com>
2019-04-14 00:31:36 -0700

    `dispatch` library now safely handles event `bind`ing and `unbind`ing during dispatch.
    Fixes issue #11.

--- dispatch.py
+++ dispatch.py
@@ -10,17 +10,65 @@ import typing
 
 
 
-class EventListeners(object):
+class RecursiveDispatchError(Exception):
+    """An exception raised when attempting to dispatch an event while it is
+    already dispatching.
+    """
+
+    event: '_EventListeners'
+    """The event that was recursively dispatched."""
+
+    def __init__(self,
+        event: '_EventListeners'
+    ) -> None:
+        self.event = event
+        super().__init__('Invalid attempt to recursively emit event {}.'.format(
+            event))
+
+
+
+
+_Callback = typing.Callable[..., typing.Optional[bool]]
+"""Type for all event listener callbacks.
+Callbacks can return `False` to halt dispatching to further listeners.
+"""
+
+
+
+
+def _remove_if_found(collection, entry):
+    try:
+        collection.remove(entry)
+    except (KeyError, ValueError):
+        pass  # Ignore if not found
+
+
+
+
+class _EventListeners(object):
     """Holds references to event names and subscribed listeners.
 
     This is used internally by :class:`Dispatcher`.
     """
-    __slots__ = ('name', '_callbacks')
+    __slots__ = ('name', '_callbacks',
+        '_calling',
+        '_pending_additions', '_pending_removals')
 
     name: str
 
-    _callbacks: typing.List[typing.Callable]
+    _callbacks: typing.List[_Callback]
     """All registered callbacks, preserving registration order."""
+
+    _calling: bool
+    """`True` while this event dispatches to its listeners."""
+
+    _pending_additions: typing.List[_Callback]
+    """Callbacks to add after `_calling` becomes `False`, preserving
+    registration order.
+    """
+
+    _pending_removals: typing.Set[_Callback]
+    """Callbacks to remove after `_calling` becomes `False`."""
 
 
     def __init__(self,
@@ -29,26 +77,60 @@ class EventListeners(object):
         self.name = name
         self._callbacks = []
 
+        self._calling = False
+        self._pending_additions = []
+        self._pending_removals = set()
+
 
     def add(self,
-        callback: typing.Callable
+        callback: _Callback
     ) -> None:
-        if callback not in self._callbacks:
-            self._callbacks.append(callback)
+        if not self._calling:
+            if callback not in self._callbacks:
+                self._callbacks.append(callback)
+
+        elif callback not in self._pending_additions:
+            # Add after done calling
+            _remove_if_found(self._pending_removals, callback)
+            if callback not in self._callbacks:
+                self._pending_additions.append(callback)
 
 
     def remove(self,
-        callback: typing.Callable
+        callback: _Callback
     ) -> None:
-        try:
-            self._callbacks.remove(callback)
-        except ValueError:
-            pass  # Ignore if callback wasn't found
+        if not self._calling:
+            _remove_if_found(self._callbacks, callback)
+
+        elif callback not in self._pending_removals:
+            # Remove after done calling
+            _remove_if_found(self._pending_additions, callback)
+            if callback in self._callbacks:
+                self._pending_removals.add(callback)
 
 
     def remove_all(self,
     ) -> None:
-        self._callbacks.clear()
+        if not self._calling:
+            self._callbacks.clear()
+
+        else:  # Remove all after done calling
+            self._pending_additions.clear()
+            self._pending_removals.update(self._callbacks)
+
+
+    def _apply_pending(self
+    ) -> None:
+        """Applies pending callback additions and removals."""
+        assert not self._calling, \
+            'Cannot apply pending callbacks while calling.'
+
+        for callback in self._pending_removals:
+            self._callbacks.remove(callback)
+        self._pending_removals.clear()
+
+        self._callbacks.extend(self._pending_additions)
+        self._pending_additions.clear()
 
 
     def __call__(self,
@@ -59,10 +141,21 @@ class EventListeners(object):
 
         Called by :meth:`~Dispatcher.emit`
         """
-        for callback in self._callbacks:
-            if callback(*args, **kwargs) is False:
-                return False
-        return True  # All listeners notified
+        if self._calling:
+            raise RecursiveDispatchError(self)
+
+        self._calling = True
+        try:
+            cancelled = False
+            for callback in self._callbacks:
+                if callback(*args, **kwargs) is False:
+                    # Don't notify any more listeners
+                    cancelled = True
+                    break
+            return not cancelled
+        finally:
+            self._calling = False
+            self._apply_pending()
 
 
     def __repr__(self
@@ -80,7 +173,7 @@ class EventListeners(object):
 class Dispatcher(object):
     """Core class used to enable all functionality in the library.
 
-    Interfaces with :class:`EventListeners` objects upon instance creation.
+    Interfaces with :class:`_EventListeners` objects upon instance creation.
 
     Events can be created by calling :meth:`register_events` or by the subclass
     definition::
@@ -94,7 +187,7 @@ class Dispatcher(object):
 
     __events_combined: typing.ClassVar[typing.Set[str]]
 
-    __event_listeners: typing.Dict[str, EventListeners]
+    __event_listeners: typing.Dict[str, _EventListeners]
 
 
     def __new__(cls,
@@ -140,12 +233,12 @@ class Dispatcher(object):
         Args:
             *names (str): Name or names of the events to register
         """
-        self.__event_listeners.update((name, EventListeners(name))
+        self.__event_listeners.update((name, _EventListeners(name))
             for name in names if name not in self.__event_listeners)
 
 
     def bind(self,
-        **event_callbacks: typing.Callable
+        **event_callbacks: _Callback
     ) -> None:
         """Subscribe to events.
 
@@ -168,7 +261,7 @@ class Dispatcher(object):
 
 
     def unbind(self,
-        *callbacks: typing.Callable
+        *callbacks: _Callback
     ) -> None:
         """Unsubscribe from events.
 
@@ -200,14 +293,14 @@ class Dispatcher(object):
 
     def get_event_listeners(self,
         name: str
-    ) -> EventListeners:
-        """Retrieve an EventListeners object by name.
+    ) -> _EventListeners:
+        """Retrieve an _EventListeners object by name.
 
         Args:
-            name (str): The name of the :class:`EventListeners` object to retrieve
+            name (str): The name of the :class:`_EventListeners` object to retrieve
 
         Returns:
-            The :class:`EventListeners` instance for the event definition
+            The :class:`_EventListeners` instance for the event definition
 
         .. versionadded:: 0.1.0
         """
@@ -226,7 +319,27 @@ if __name__ == '__main__':
             pass
 
     test = Test()
-    test.bind(EVENT_A=print, EVENT_B=print)
+
+    def b_listener_3(
+        msg: str
+    ) -> None:
+        print('b_listener_3', msg)
+
+    def b_listener_1(
+        msg: str
+    ) -> None:
+        print('b_listener_1', msg)
+        test.unbind(b_listener_1)
+        test.bind(EVENT_B=b_listener_3)
+
+    def b_listener_2(
+        msg: str
+    ) -> None:
+        print('b_listener_2', msg)
+
+    test.bind(EVENT_A=print)
+    test.bind(EVENT_B=b_listener_1)
+    test.bind(EVENT_B=b_listener_2)
     test.emit('EVENT_A', 'Event A fired.')
-    test.emit('EVENT_B', 'Event B fired.')
-    print(repr(test), '=', dir(test))
+    test.emit('EVENT_B', 'Event B-1 fired.')
+    test.emit('EVENT_B', 'Event B-2 fired.')
