cb3687ec8d50c0378fbe488d6139ec675ec18404
Erik Anderson <anderikson1@gmail.com>
2019-04-16 10:14:57 -0700

    Initial commit: Added print statements to showcase EasyMoney functionality

--- view/window_view.py
+++ view/window_view.py
@@ -40,10 +40,65 @@ class WindowView(object):
     ) -> None:
         """
         """
-        pass
+        # WindowView(SimController(SimModel())).run()
+        print('Welcome to EasyMoney')
 
+        # TRADER
+        names = ['Madoff', 'Belfort', 'Stewart']
+        initial_funds: float = 10000.0
+        trading_fee: float = .05
+        momentum_trader = MomentumTrader()
+        algorithm: str = momentum_trader.get_algorithm_name()
+        algorithm_settings: dict = momentum_trader.get_algorithm_settings_defaults()
+        print('Adding traders')
+        try:
+            for name in names:
+                self._sim_controller.add_trader(name,
+                                                initial_funds,
+                                                trading_fee,
+                                                algorithm,
+                                                algorithm_settings)
+        except Exception as e:
+            print(e)
+        print('Traders Madoff, Belfort, Stewart successfully added.\n')
 
+        # DATASOURCE
+        file_names = ['AAPL.json', 'MSFT.json', 'AMD.json', 'JCOM.json']
+        print('Adding datasources.')
+        for filename in file_names:
+            try:
+                self._sim_controller.get_datasource().add_stock_symbol(filename)
+            except Exception as e:
+                print(e)
+        try:
+            if self._sim_controller.get_datasource().can_confirm():
+                self._sim_controller.get_datasource().confirm()
+        except Exception as e:
+            print(e)
+        print('Datasources successfully added.\n')
 
+        # SIMULATION
+        print('Starting simulation.')
+        try:
+            self._sim_controller.get_updater().play()
+        except Exception as e:
+            print(e)
+        print('Simulation complete.\n')
+
+        # STATISTICS
+        print('Statistics display:')
+        trader_list = self._sim_controller.get_model().get_traders()
+        for trader in trader_list:
+            try:
+                print('Trader: ' + trader.get_name())
+                print(trader.get_account().get_statistics_overall())
+                print()
+            except Exception as e:
+                print(e)
+
+        print('\nThank you for using EasyMoney.')
 
 # Imported last to avoid circular dependencies
 from controller.sim_controller import SimController
+from model.algorithms.momentum_trader import MomentumTrader
+
