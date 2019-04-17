f5ff247eacf642411656a8a728c863c7f1773d63
James Abernathy <jabernat@gmail.com>
2019-04-17 10:57:28 -0700

    Added event printouts for everything but `TraderAccount`s.

--- view/window_view.py
+++ view/window_view.py
@@ -13,6 +13,8 @@ from kivy.lang import Builder
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
 
+import dispatch
+
 # Local package imports duplicated at end of file to resolve circular dependencies
 if typing.TYPE_CHECKING:
     from controller.sim_controller import SimController
@@ -85,11 +87,29 @@ class WindowView(App):
         import pprint
         pprinter = pprint.PrettyPrinter(indent=4)
 
+        def create_event_printer(
+            event_name: str
+        ) -> typing.Callable[..., None]:
+            def event_printer(*args, **kwargs):
+                kwargs.update(enumerate(args))
+                arguments = pprinter.pformat(kwargs)
+                print('{} = \\\n{}'.format(event_name, arguments))
+            return event_printer
+
+        def print_all_events(
+            dispatcher: dispatch.Dispatcher
+        ) -> None:
+            dispatcher.bind(**{event_name: create_event_printer(event_name)
+                for event_name in dispatcher.EVENTS})
+
         print('Welcome to EasyMoney')
         controller = self.get_controller()
-        model = controller.get_model()
+        print_all_events(controller.get_datasource())
+        print_all_events(controller.get_updater())
 
-        # TODO: Register for all events
+        model = controller.get_model()
+        print_all_events(model)
+        print_all_events(model.get_stock_market())
 
         print('Adding traders')
         ALGORITHM = 'Momentum'
@@ -97,9 +117,11 @@ class WindowView(App):
         TRADING_FEE = 0.50
         algorithm_settings = model.get_trader_algorithm_settings_defaults(ALGORITHM)
         for name in ['Madoff', 'Belfort', 'Stewart']:
-            self._sim_controller.add_trader(name,
+            trader = self._sim_controller.add_trader(name,
                 initial_funds=INITIAL_FUNDS, trading_fee=TRADING_FEE,
                 algorithm=ALGORITHM, algorithm_settings=algorithm_settings)
+            print_all_events(trader)
+            # TODO: Register for TRADER_ACCOUNT_CREATED to register for account's events
 
         print('Adding datasources')
         for filename in [
