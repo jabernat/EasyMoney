50233762c0e21ea16f36cade2a468006487e3fb6
James Abernathy <jabernat@gmail.com>
2019-05-04 19:37:53 -0700

    Moved Kivy settings and logs to a local `.kivy` folder.

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ __copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Ge
 __license__ = 'MIT'
 
 
+import os.path
+import sys
+
 def pythonw_disable_std_streams(
 ) -> None:
     """Disable output streams when running in console-less mode on Windows
     (using `pythonw`) to prevent a crash when Kivy attempts to print to
     unopened `stdout` and `stderr`.
     """
-    import os.path
-    import sys
-
     python_filename_without_extension = os.path.splitext(
         os.path.basename(sys.executable))[0] 
     if python_filename_without_extension == 'pythonw':
@@ -26,6 +26,10 @@ if __name__ == '__main__':
     # Disable streams before anything attempts to print
     pythonw_disable_std_streams()
 
+
+# Store Kivy configuration and logs in a local folder
+os.environ['KIVY_HOME'] = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '.kivy')
 
 import kivy
 kivy.require('1.9.0')
