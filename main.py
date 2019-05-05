9e6802642b5e1d5d1d0e9dbcb37ed088a0b24449
James Abernathy <jabernat@gmail.com>
2019-05-04 19:11:09 -0700

    Open null `stdout` and `stderr` pipes when running in console-less mode on Windows.
    This avoids an error when anything attempts to `print` to those pipes when they're unopened.

--- main.py
+++ main.py
@@ -6,6 +6,27 @@ __copyright__ = 'Copyright © 2019, Erik Anderson, James Abernathy, and Tyler Ge
 __license__ = 'MIT'
 
 
+def pythonw_disable_std_streams(
+) -> None:
+    """Disable output streams when running in console-less mode on Windows
+    (using `pythonw`) to prevent a crash when Kivy attempts to print to
+    unopened `stdout` and `stderr`.
+    """
+    import os.path
+    import sys
+
+    python_filename_without_extension = os.path.splitext(
+        os.path.basename(sys.executable))[0] 
+    if python_filename_without_extension == 'pythonw':
+        # Interpreter hasn't opened standard pipes
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+
+if __name__ == '__main__':
+    # Disable streams before anything attempts to print
+    pythonw_disable_std_streams()
+
+
 from model.sim_model import SimModel
 from controller.sim_controller import SimController
 from view.logging_view import LoggingView
