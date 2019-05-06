a031803901f0d5688b29b1a2bcf3cff437312b16
Tyler Gerritsen <GylerTerritsen@gmail.com>
2019-05-05 22:41:59 -0700

    Working prototype
    This branch still isn't done. The new goal is to make the list of
    symbols dynamic, and some resizing needs to be done to make the
    layouts look better. Nevertheless, this module does now function for
    the symbols specified in our requirements document.

--- view/stock_symbols_tab.py
+++ view/stock_symbols_tab.py
@@ -7,40 +7,47 @@ __license__ = 'MIT'
 
 import typing
 
-from kivy.app import App
+
+from kivy.properties import ObjectProperty
+from kivy.properties import ListProperty
+from kivy.app import Builder
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanelItem
-from kivy.lang import Builder
+from kivy.uix.button import Button
 
 # Local package imports duplicated at end of file to resolve circular dependencies
 if typing.TYPE_CHECKING:
     #from controller.sim_controller import SimController
     pass
 
-
-
-
 class StockSymbolsTab(TabbedPanelItem):
     """Class associated with the `<StockSymbolsTab>` template defined within
     `stock_symbols_tab.kv`.
     """
 
-    def __init__(self):
-        super.__init__()
-        Builder.load_file('view/stock_symbols_tab.kv')
+    stock_symbol: str
 
-    def on_add_button_press(self
-        ) -> None:
+    # References to component widgets
+    button_add: Button
+    button_remove: Button
+
+    def on_add_button_click(self,
+        controller: 'SimController'
+    ) -> None:
         """
         Method for a press of the add button on the GUI. Checks if there are
         any symbols left to add to the simulation. If not, informs the user
         of that fact. If there is an available symbol to add, opens a dialog
         to choose which symbol.
-        :return:
         """
-        pass
+        filename = self.stock_symbol + ".json"
+        controller.get_datasource().add_stock_symbol(filename)
 
-    def on_remove_button_press(self
-        ) -> None:
+    def on_remove_button_click(self,
+        controller: 'SimController'
+    ) -> None:
         """
         Method for a press of the remove button on the GUI. Checks if there are
         any symbols left to remove from the simulation. If not, informs the user
@@ -48,19 +55,17 @@ class StockSymbolsTab(TabbedPanelItem):
         highlighted by the user. If no symbol is highlighted, the user
         will be informed of that fact and prompted to select a symbol to remove.
         """
-        pass
+        controller.get_datasource().remove_stock_symbol(self.stock_symbol)
 
     def on_symbol_label_select(self,
-        stock_symbol: str
-        ) -> None:
+        symbol: str
+    ) -> None:
         """
-        When a user clicks on a stock symbol on the list, the symbol should highlight
+        When a user clicks on a stock symbol on the list, the symbol should be highlighted
         and appropriate data keeping track of that action should be set. The goal is,
         if the remove button is pressed, it should remove the selected symbol.
         """
-        pass
-
-
+        self.stock_symbol = symbol
 
 
 
