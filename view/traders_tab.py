6904b6d2fd65820e142a0302dd8e9e43997c53bb
James Abernathy <jabernat@gmail.com>
2019-05-08 17:14:06 -0700

    Implemented most of add/edit trader popup, except for real validation and saving changes.

--- view/traders_tab.py
+++ view/traders_tab.py
@@ -11,10 +11,11 @@ from kivy.app import App
 from kivy.properties import (
     NumericProperty, ObjectProperty, StringProperty)
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
 from kivy.uix.tabbedpanel import TabbedPanelItem
+from kivy.uix.textinput import TextInput
 
 # Local package imports duplicated at end of file to resolve circular dependencies
 if typing.TYPE_CHECKING:
@@ -26,7 +27,63 @@ if typing.TYPE_CHECKING:
 
 class TraderPopup(Popup):
     """Popup dialog for adding and editing traders."""
-    pass
+
+
+    trader: typing.Optional['Trader'] = ObjectProperty(None, allownone=True)
+    """The `Trader` to edit, or `None` to add a new trader."""
+
+    # References to component widgets
+    input_name: TextInput
+    input_algorithm: Spinner
+    input_initial_funds: TextInput
+    input_trading_fee: TextInput
+
+
+    def on_open(self,
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> None:
+        """Prepare the popup when opened."""
+        # Focus first control automatically
+        first_control = (self.input_name if self.trader is None
+            else self.input_initial_funds)
+        first_control.focus = True
+
+        # Force initial validation, which doesn't trigger when .trader is set
+        self.validate_name()
+        self.validate_algorithm()
+        self.validate_initial_funds()
+        self.validate_trading_fee()
+
+
+    def validate_name(self
+    ) -> None:
+        input = self.input_name
+        input.valid = len(input.text) > 0
+
+
+    def validate_algorithm(self
+    ) -> None:
+        input = self.input_algorithm
+        input.valid = len(input.text) > 0
+
+
+    def validate_initial_funds(self
+    ) -> None:
+        input = self.input_initial_funds
+        input.valid = len(input.text) > 0
+
+
+    def validate_trading_fee(self
+    ) -> None:
+        input = self.input_trading_fee
+        input.valid = len(input.text) > 0
+
+
+    def save(self
+    ) -> None:
+        """Applies valid trader info before closing the popup."""
+        self.dismiss()
 
 
 
@@ -45,7 +102,8 @@ class TraderRow(FloatLayout):
 
 
     def __init__(self,
-        *args, **kwargs
+        *args: typing.Any,
+        **kwargs: typing.Any
     ) -> None:
         super().__init__(*args, **kwargs)
 
@@ -81,12 +139,9 @@ class TradersTab(TabbedPanelItem):
 
 
     # References to component widgets
-    button_add: Button
-    button_edit: Button
-    button_remove: Button
     table_rows: BoxLayout
 
-    selected_trader: typing.Optional[TraderRow] = ObjectProperty(
+    selected_trader_row: typing.Optional[TraderRow] = ObjectProperty(
         None, allownone=True)
     """The selected trader's table row, or `None` when unselected."""
 
@@ -95,7 +150,8 @@ class TradersTab(TabbedPanelItem):
 
 
     def __init__(self,
-        *args, **kwargs
+        *args: typing.Any,
+        **kwargs: typing.Any
     ) -> None:
         super().__init__(*args, **kwargs)
 
@@ -114,30 +170,10 @@ class TradersTab(TabbedPanelItem):
                 trader=trader)
 
 
-    @staticmethod
-    def on_selected_trader(
-        instance: 'TradersTab',
-        selected_trader: typing.Optional[TraderRow]
-    ) -> None:
-        """Disable edit and remove buttons when no trader is selected.
-
-        Note: `selected_trader` is an `ObjectProperty`, which does not
-        automatically trigger this handler when instantiated.
-        """
-        buttons_disabled = selected_trader is None
-
-        instance.button_edit.disabled = buttons_disabled
-        instance.button_remove.disabled = buttons_disabled
-
-
-    def on_button_add_clicked(self
+    def on_add_clicked(self
     ) -> None:
         """Show a popup to add a new trader."""
-        controller = App.get_running_app().get_controller()
-        for index in range(1, 21):
-            controller.add_trader('John Madden {}'.format(index),
-                initial_funds=999_999, trading_fee=0.01,
-                algorithm='Momentum')
+        TraderPopup(trader=None).open()
 
     def on_simmodel_trader_added(self,
         model: 'SimModel',
@@ -154,24 +190,27 @@ class TradersTab(TabbedPanelItem):
         self.trader_names_to_rows[trader.get_name()] = trader_row
 
 
-    def on_button_edit_clicked(self
+    def on_edit_clicked(self
     ) -> None:
-        """Show a popup to edit `selected_trader`."""
-        assert self.selected_trader is not None, \
+        """Show a popup to edit `selected_trader_row`."""
+        assert self.selected_trader_row is not None, \
             'Edit button clicked without a trader row selected.'
 
-        TraderPopup(title='Edit Trader').open()
+        model = App.get_running_app().get_controller().get_model()
+        trader = model.get_trader(self.selected_trader_row.trader_name)
+
+        TraderPopup(trader=trader).open()
 
 
-    def on_button_remove_clicked(self
+    def on_remove_clicked(self
     ) -> None:
-        """Remove `selected_trader`."""
-        assert self.selected_trader is not None, \
+        """Remove `selected_trader_row`."""
+        assert self.selected_trader_row is not None, \
             'Remove button clicked without a trader row selected.'
 
         controller = App.get_running_app().get_controller()
         controller.remove_trader(
-            self.selected_trader.trader_name)
+            self.selected_trader_row.trader_name)
 
     def on_simmodel_trader_removed(self,
         model: 'SimModel',
@@ -183,8 +222,8 @@ class TradersTab(TabbedPanelItem):
         self.table_rows.remove_widget(trader_row)
         del self.trader_names_to_rows[trader.get_name()]
 
-        if self.selected_trader == trader_row:
-            self.selected_trader = None
+        if self.selected_trader_row == trader_row:
+            self.selected_trader_row = None
 
 
 
