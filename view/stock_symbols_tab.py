fd6713908a94120cdd0e50ba19f2914279fa29e7
Tyler Gerritsen <GylerTerritsen@gmail.com>
2019-05-10 01:45:02 -0700

    Working model to debug
    This now adds and removes traders into the datasource... Sort of. The only problem is a type error in the add and remove events from the datasource. Once that bug is fixed, this tab should run properly. I've also cleaned up the comments and code. It looks better now.

--- view/stock_symbols_tab.py
+++ view/stock_symbols_tab.py
@@ -9,7 +9,7 @@ import typing
 
 from kivy.app import App
 from kivy.properties import (
-    NumericProperty, ObjectProperty, StringProperty)
+    NumericProperty, ObjectProperty, StringProperty, ListProperty)
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
@@ -25,104 +25,32 @@ if typing.TYPE_CHECKING:
     from model.trader import Trader
 
 
-
-
 class FilePopup(Popup):
     """Popup dialog for adding symbols."""
 
-    # References to component widgets
-    stock_exchange: TextInput
-    symbol_name: str
-
-    def on_open(self,
-        *args: typing.Any,
-        **kwargs: typing.Any
+    def _open(self,
+        file: str
     ) -> None:
-        """Prepare the popup when opened."""
-
-    '''
-    def validate_name(self
-    ) -> None:
-        controller = App.get_running_app().get_controller()
-
-        input = self.input_name
-        if self.trader is not None:  # Name in use by this trader
-            input.valid = True  # Changing name isn't allowed anyway
-            return
-
-        input.valid, invalid_reason = controller.validate_trader_name(
-            input.text)
-
-
-    def validate_algorithm(self
-    ) -> None:
-        controller = App.get_running_app().get_controller()
-
-        input = self.input_algorithm
-        input.valid, invalid_reason = controller.validate_trader_algorithm(
-            input.text)
-
-
-    def validate_initial_funds(self
-    ) -> None:
-        controller = App.get_running_app().get_controller()
-
-        input = self.input_initial_funds
-        input.valid, invalid_reason = controller.validate_trader_initial_funds(
-            input.text)
-
-
-    def validate_trading_fee(self
-    ) -> None:
-        controller = App.get_running_app().get_controller()
-
-        input = self.input_trading_fee
-        input.valid, invalid_reason = controller.validate_trader_trading_fee(
-            input.text)
-
-
-    def _save(self
-    ) -> None:
-        """Apply presumably valid trader info before closing the popup."""
-        controller = App.get_running_app().get_controller()
-
-        if self.trader is None:  # Create new trader
-            controller.add_trader(
-                name=self.input_name.text,
-                initial_funds=self.input_initial_funds.text,
-                trading_fee=self.input_trading_fee.text,
-                algorithm=self.input_algorithm.text)
-
-        else:  # Edit existing trader
-            trader_name = self.trader.get_name()
-            controller.set_trader_initial_funds(
-                trader_name, self.input_initial_funds.text)
-            controller.set_trader_trading_fee(
-                trader_name, self.input_trading_fee.text)
-
+        """Apply presumably valid symbol info before closing the popup."""
+        datasource = App.get_running_app().get_controller().get_datasource()
+        datasource.add_stock_symbol(file);
         self.dismiss()
-        '''
-
-
 
 
 class SymbolRow(FloatLayout):
-    """Selectable table body row representing a trader."""
-
+    """Selectable table body row representing a symbol."""
 
     tab: 'StockSymbolsTab' = ObjectProperty()
     """Reference to parent tab for tracking the selected row."""
 
     symbol_name: str = StringProperty()
-    symbol_exchange: str = StringProperty()
 
     def __init__(self,
-        *args: typing.Any,
-        **kwargs: typing.Any
+        symbol: str
     ) -> None:
-        super().__init__(*args, **kwargs)
+        super().__init__(symbol)
 
-        datasource = App.get_running_app().get_controller().get_datasource()
+        self.symbol_name = symbol
         assert self.symbol_name is not None, \
             'Attempt to create table row for non-existent symbol.'
 
@@ -149,62 +77,52 @@ class StockSymbolsTab(TabbedPanelItem):
     ) -> None:
         super().__init__(*args, **kwargs)
 
-        self.trader_names_to_rows = {}
+        self.symbol_names_to_rows = {}
 
-        model = App.get_running_app().get_controller().get_model()
+        datasource = App.get_running_app().get_controller().get_datasource()
 
-        model.bind(
-            SIMMODEL_TRADER_ADDED=self.on_simmodel_trader_added,
-            SIMMODEL_TRADER_REMOVED=self.on_simmodel_trader_removed)
-
-        # Add preexisting traders
-        for trader in model.get_traders():
-            self.on_simmodel_trader_added(
-                model=model,
-                trader=trader)
-
+        datasource.bind(
+            MARKETDATASOURCE_STOCK_SYMBOL_ADDED=self.on_datasource_symbol_added,
+            MARKETDATASOURCE_STOCK_SYMBOL_REMOVED=self.on_datasource_symbol_removed)
 
     def on_add_clicked(self
     ) -> None:
-        """Show a popup to add a new trader."""
+        """Show a popup to add a new symbol."""
         FilePopup().open()
 
-    def on_simmodel_trader_added(self,
-        model: 'SimModel',
-        trader: 'Trader'
+    def on_datasource_symbol_added(self,
+        datasource: 'MarketDatasource',
+        symbol: str
     ) -> None:
-        """Add a table row for the newly added `trader`."""
-        symbol_row = SymbolRow(tab=self,
-            trader_name=trader.get_name(),
-            trading_algorithm=trader.get_algorithm_name(),
-            initial_funds=trader.get_initial_funds(),
-            trading_fee=trader.get_trading_fee())
+
+        """Add a table row for the newly added symbol."""
+        symbol_row = SymbolRow(symbol)
 
         self.table_rows.add_widget(symbol_row)
-        self.trader_names_to_rows[trader.get_name()] = symbol_row
+        self.symbol_names_to_rows[symbol] = symbol_row
 
     def on_remove_clicked(self
     ) -> None:
-        """Remove `selected_trader_row`."""
-        assert self.selected_trader_row is not None, \
-            'Remove button clicked without a trader row selected.'
+        """Remove `selected_symbol_row`."""
+        assert self.selected_symbol_row is not None, \
+            'Remove button clicked without a symbol row selected.'
 
-        controller = App.get_running_app().get_controller()
-        controller.remove_trader(
-            self.selected_trader_row.trader_name)
+        datasource = App.get_running_app().get_controller().get_datasource()
+        datasource.remove_stock_symbol(
+            self.selected_symbol_row.symbol_name)
 
-    def on_simmodel_trader_removed(self,
-        model: 'SimModel',
-        trader: 'Trader'
+    def on_datasource_symbol_removed(self,
+        datasource: 'MarketDatasource',
+        symbol: str
     ) -> None:
-        """Remove the table row for deleted `trader`."""
-        trader_row = self.trader_names_to_rows[trader.get_name()]
+        """Remove the table row for deleted `symbol`."""
+        symbol_row = self.trader_names_to_rows[symbol]
 
-        self.table_rows.remove_widget(trader_row)
-        del self.trader_names_to_rows[trader.get_name()]
+        self.table_rows.remove_widget(symbol)
+        del self.trader_names_to_rows[symbol]
 
-        if self.selected_trader_row == trader_row:
-            self.selected_trader_row = None
+        if self.selected_symbol_row == symbol_row:
+            self.selected_symbol_row = None
 
 
 
